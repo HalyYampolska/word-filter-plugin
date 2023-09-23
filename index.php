1f0d560e1aa1a7eecb43a88be7a7b17765d64583
Halyna Yampolska <50921848+HalyYampolska@users.noreply.github.com>
2023-09-23 22:21:28 +0300

    Added sun menu

--- index.php
+++ index.php
@@ -12,8 +12,25 @@ if( ! defined ( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class OurWordFilterPlugin {
     function __construct() {
-        add_action('admin_menu');
+        add_action('admin_menu', array($this, 'ourMenu'));
     }
+
+    function ourMenu() {
+        add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', 
+        array($this, 'wordFilterPage'), 'dashicons-smiley', 100);
+        add_submenu_page('ourwordfilter','Words To Filter','Word List','manage_options',
+        'ourwordfilter', array($this, 'wordFilterPage'));
+        add_submenu_page('ourwordfilter','Word Filter Options','Options','manage_options',
+        'word-filter-options', array($this, 'optionsSubPage'));
+    }
+
+    function wordFilterPage() { ?>
+        Hello world
+    <?php }
+
+    function optionsSubPage() { ?>
+        Hello world from the options page
+    <?php }
 }
 
 $ourWordFilterPlugin = new OurWordFilterPlugin();
